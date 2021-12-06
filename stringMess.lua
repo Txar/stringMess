a3bf800a6ba0323ed0f9182ed38a38a1aefbbe2c
Txar <67731745+Txar@users.noreply.github.com>
2021-12-06 22:00:03 +0100

    Create stringMess.lua

---
+++ stringMess.lua
@@ -0,0 +1,69 @@
+--[[
+function love.draw()
+    love.graphics.print("Hello World", 400, 300)
+end
+--]]
+function getList(str)
+    strl = {}
+    for i in string.gmatch(str, "%S+") do
+        strl[#(strl) + 1] = i
+    end
+    return strl
+end
+
+function getMessedString(strl)
+    str2 = ""
+    for i=1,#(strl) do
+        str2 = str2..messUp(strl[i])
+        if (i ~= #(strl)) then
+            str2 = str2.." "
+        end
+    end
+    return str2
+end
+
+function getString(strl)
+    str2 = ""
+    for i=1,#(strl) do
+        str2 = str2..strl[i]
+        if (i ~= #(strl)) then
+            str2 = str2.." "
+        end
+    end
+    return str2
+end
+
+function messUp(str)
+    str2 = ""
+    g = string.len(str)
+    for i=1,g do
+        ing = math.random(string.len(str))
+        str2 = str2..string.sub(str, ing, ing)
+        str = string.sub(str, 1, ing - 1)..string.sub(str, ing + 1, g)
+    end
+    return str2
+end
+
+function advancedMess(str)
+    str2 = ""
+    strl = getList(str)
+    str2 = getMessedString(strl)
+    return str2
+end
+
+function wordMess(str)
+    strl = getList(str)
+    strl2 = {}
+    for i=1,#(strl) do
+        ii = math.random(#(strl))
+        strl2[#(strl2) + 1] = strl[ii]
+        table.remove(strl, ii)
+    end
+    str2 = getString(strl2)
+    return str2
+end
+
+str = "what a nice string you got there would be a shame if someone messed it up"
+print(messUp(str))
+print(advancedMess(str))
+print(wordMess(str))
